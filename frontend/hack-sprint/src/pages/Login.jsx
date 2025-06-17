700de5726212fd32470d47dd313158d6bd25428e
rahul-1901 <iitdelhi98@gmail.com>
2025-06-17 17:03:19 +0530

    feat: add GitHub authentication controller & new dependencies

--- frontend/hack-sprint/src/pages/Login.jsx
+++ frontend/hack-sprint/src/pages/Login.jsx
@@ -27,13 +27,17 @@ const Login = () => {
         flow: 'auth-code'
     })
 
+    const githubAuthLogin = () => {
+        window.location.assign("https://github.com/login/oauth/authorize?client_id=" + import.meta.env.VITE_GITHUB_CLIENT_ID)
+    }
+
     return (
         <>
             <div className='min-h-screen flex justify-center items-center bg-gray-900'>
                 <div className='flex flex-col gap-5 items-center'>
                     <button onClick={googleLogin} className='text-white border-2 border-green-800 px-4 py-2 rounded-xl cursor-pointer'>Google Login</button>
                     <p className='text-white'>------Or-------</p>
-                    <button onClick={googleLogin} className='text-white border-2 border-green-800 px-4 py-2 rounded-xl cursor-pointer'>Github Login</button>
+                    <button onClick={githubAuthLogin} className='text-white border-2 border-green-800 px-4 py-2 rounded-xl cursor-pointer'>Github Login</button>
                 </div>
             </div>
         </>
