070f1fce9a95ee2005fa1a89b3748ea8f1e86b42
Mohit Gupta <mannugupta2005@gmail.com>
2025-06-19 00:02:52 +0530

    adding feature of sending expired hackathones

--- backend/controllers/hackathon.controllers.js
+++ backend/controllers/hackathon.controllers.js
@@ -1,11 +1,27 @@
 import mongoose from "mongoose"
 import express from 'express'
 import hackathonModel from "../models/hackathon.models.js"
+import { all } from "axios"
 
-export const sendHackathons = async () => {
+export const sendHackathons = async (req,res,next) => {
     try {
         const allHackathons = hackathonModel.find({
             status: true
+        })
+        res.status(200).json({
+            allHackathons
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        })     
+    }
+}
+export const sendExpiredHackathons = async (req,res,next) => {
+    try {
+        const allHackathons = await hackathonModel.find({
+            status: false,
+            endDate : {$lt : new Date(Date.now())}
         })
         res.status(200).json({
             allHackathons
