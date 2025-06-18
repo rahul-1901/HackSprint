b5903ddbdaa1457075ddb3f91b7af06182fba27c
Mohit Gupta <mannugupta2005@gmail.com>
2025-06-19 00:04:16 +0530

    adding route for displaying expired hackathons

--- backend/routes/hackathon.routes.js
+++ backend/routes/hackathon.routes.js
@@ -1,9 +1,9 @@
 import express from 'express'
 import { Router } from 'express'
-import { sendHackathons } from '../controllers/hackathon.controllers.js'
+import { sendHackathons,sendExpiredHackathons} from '../controllers/hackathon.controllers.js'
 
 const hackathonRoutes = Router();
 
 hackathonRoutes.get("/", sendHackathons)
-
+hackathonRoutes.get("/expiredHackathons",sendExpiredHackathons)
 export default hackathonRoutes
