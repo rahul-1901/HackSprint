f2a65e4ceb5d97beca7f46b126e70efd2fe50215
Ridham Shah <ridhamshah1002@gmail.com>
2025-07-12 12:03:55 +0530

    Styled dashboard using dummy data

--- backend/controllers/hackathon.controllers.js
+++ backend/controllers/hackathon.controllers.js
@@ -31,3 +31,21 @@ export const sendInactiveHackathons = async (req,res) => {
         })     
     }
 }
+// above code sent all inactive hackathons while below code sent only expired hackathons
+export const sendExpiredHackathons = async(req,res)=>{
+    try{
+        const currentTime = new Date(Date.now())
+        const expiredHackathons = await hackathonModel.find({
+            status : false,
+            endDate : {$lt : currentTime}
+        })
+
+        res.status(200).json({
+            expiredHackathons
+        })
+    }catch(error){
+        res.status(500).json({
+            message : error.message
+        })
+    }
+}
--- backend/routes/hackathon.routes.js
+++ backend/routes/hackathon.routes.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import { Router } from 'express'
-import { sendHackathons,sendInactiveHackathons} from '../controllers/hackathon.controllers.js'
+import { sendExpiredHackathons, sendHackathons,sendInactiveHackathons} from '../controllers/hackathon.controllers.js'
 
 const hackathonRoutes = Router();
 
 hackathonRoutes.get("/", sendHackathons)
 hackathonRoutes.get("/inactiveHackathons",sendInactiveHackathons)
+hackathonRoutes.get("/expiredHackathons",sendExpiredHackathons)
 export default hackathonRoutes
