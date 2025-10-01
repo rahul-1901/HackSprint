bfa0d4e37f78c0de13c5ec212204b79d5ef9a70f
rahul-1901 <iitdelhi98@gmail.com>
2025-10-01 14:43:01 +0530

    repeatedLogic

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -261,12 +261,7 @@ const googleLogin = async (req, res) => {
     );
 
     const { name, email } = userRes.data;
-    if (!isIITJEmail(email)) {
-      return res.status(403).json({
-        message: "Only iitj emails are allowed.",
-        success: false,
-      });
-    }
+    
     if (!isIITJEmail(email)) {
       return res.status(403).json({
         message: "Only iitj emails are allowed.",
