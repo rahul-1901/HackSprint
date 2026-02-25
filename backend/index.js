d010679c5b573114babca29276bf9cb288cc5458
rahul-1901 <iitdelhi98@gmail.com>
2026-02-25 15:47:28 +0530

    corsIssue

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ const app = express()
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:3000", "http://127.0.0.1:5173"],
+    origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:3000", "http://127.0.0.1:5173", "https://hack-sprint-iitj.vercel.app"],
     methods: ["GET", "POST"],
     credentials: true
   },
@@ -84,7 +84,7 @@ dotenv.config()
 connectDB()
 
 const corsOptions = {
-    origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:3000", "http://127.0.0.1:5173"],
+    origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:3000", "http://127.0.0.1:5173", "https://hack-sprint-iitj.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
     optionsSuccessStatus: 200
